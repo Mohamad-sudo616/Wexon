f2d5502fd0e99c1d22053b8cbe1d79bd3b2f9837
Mohamad-sudo616 <mohamadamini4884@gmail.com>
2026-08-29 14:24:14 +0330

    log in log out completed

--- backend/apps/core/views.py
+++ backend/apps/core/views.py
@@ -1,6 +1,6 @@
-from django.contrib.auth import login
+from django.contrib.auth import login, logout
 from django.shortcuts import redirect, render
-
+from django.contrib.auth.forms import AuthenticationForm
 from apps.products.models import Product
 
 from .forms import RegistrationForm
@@ -39,3 +39,36 @@ def register(request):
             "form": form,
         },
     )
+
+
+def login_view(request):
+    if request.user.is_authenticated:
+        return redirect("home")
+
+    if request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)
+
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+
+            return redirect("home")
+
+    else:
+        form = AuthenticationForm()
+
+    return render(
+        request,
+        "pages/login.html",
+        {
+            "form": form,
+        },
+    )
+
+
+
+
+def logout_view(request):
+    logout(request)
+
+    return redirect("home")
