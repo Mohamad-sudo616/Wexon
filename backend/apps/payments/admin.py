36b871b835e67773e7b13b51f0b0b3b0e64c0a8b
Mohamad-sudo616 <mohamadamini4884@gmail.com>
2026-09-23 12:49:55 +0330

    chnages

--- backend/apps/payments/admin.py
+++ backend/apps/payments/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Payment
+
+
+@admin.register(Payment)
+class PaymentAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "order",
+        "amount",
+        "status",
+        "authority",
+        "ref_id",
+        "created_at",
+    )
+
+    list_filter = (
+        "status",
+        "created_at",
+    )
+
+    search_fields = (
+        "authority",
+        "ref_id",
+        "order__full_name",
+        "order__email",
+    )
+
+    readonly_fields = (
+        "created_at",
+        "updated_at",
+    )
