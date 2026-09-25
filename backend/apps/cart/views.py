bd7bc4d339e94d72262412a3a35b7570ac9c254e
Mohamad-sudo616 <mohamadamini4884@gmail.com>
2026-09-25 21:25:17 +0330

    new changes testing

--- backend/apps/cart/views.py
+++ backend/apps/cart/views.py
@@ -1,4 +1,3 @@
-from django.contrib.auth.decorators import login_required
 from django.shortcuts import get_object_or_404, redirect, render
 
 from apps.products.models import Product
@@ -7,13 +6,16 @@ from .models import Cart, CartItem
 
 
 def add_to_cart(request, product_id):
-    product = get_object_or_404(Product, id=product_id)
+    product = get_object_or_404(
+        Product,
+        id=product_id,
+        is_available=True,
+    )
 
     if request.user.is_authenticated:
         cart, created = Cart.objects.get_or_create(
             user=request.user,
         )
-
     else:
         if not request.session.session_key:
             request.session.create()
@@ -29,9 +31,17 @@ def add_to_cart(request, product_id):
         product=product,
     )
 
-    if not created:
-        cart_item.quantity += 1
-        cart_item.save()
+    if created:
+        if product.stock <= 0:
+            cart_item.delete()
+        else:
+            cart_item.quantity = 1
+            cart_item.save()
+
+    else:
+        if cart_item.quantity < product.stock:
+            cart_item.quantity += 1
+            cart_item.save()
 
     return redirect("cart:detail")
 
@@ -41,9 +51,6 @@ def cart_detail(request):
         cart, created = Cart.objects.get_or_create(
             user=request.user,
         )
-
-
-
     else:
         if not request.session.session_key:
             request.session.create()
@@ -54,17 +61,16 @@ def cart_detail(request):
             session_key=session_key,
         )
 
-
     cart_total = sum(
         item.total_price
         for item in cart.items.select_related("product")
-)
+    )
 
     return render(
         request,
         "pages/cart_detail.html",
         {
             "cart": cart,
-            "cart_total":cart_total
-       },
+            "cart_total": cart_total,
+        },
     )
